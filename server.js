882f5174fb79698bd2d2dab5d4a05d2af704398b
Shari Hobbs <sharirhobbs@gmail.com>
2018-05-20 12:42:35 -0700

    added cors config

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
- const express = require('express');
- const app = express();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 
- const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
- module.exports = {app};
+app.get('/api/*', (req, res) => {
+ res.json({ok: true});
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
