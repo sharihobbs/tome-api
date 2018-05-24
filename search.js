bda382343fd1ce16b2f49f6be6e1c08df8dc1178
Shari Hobbs <sharirhobbs@gmail.com>
2018-05-24 13:02:48 -0700

    changed api call search limit and added dedupe function to results

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ const books = require('google-books-search');
 const Promise = require('bluebird')
 const _ = require('lodash')
 
-const SEARCH_LIMIT = 9
+const SEARCH_LIMIT = 36
 const options = {
     key: config.API_KEY,
     offset: 0,
@@ -13,17 +13,25 @@ const options = {
     lang: 'en'
 };
 
+function removeDuplicates(array, prop) {
+  return array.filter((obj, pos, arr) => {
+    return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop] === pos)
+  })
+}
+
 function searchBooks(query, page) {
   options.offset = !page || page === 1 ? 0 : offsetForPage(page, SEARCH_LIMIT)
   return new Promise(function (resolve, reject) {
     books.search(query, options, function(err, results) {
+      let googleBooks = [];
       if (err) {
         return reject(err)
       } else {
         _.map(results, book => {
-          console.log(options.offset, book.id, book.title, book.authors, book.industryIdentifiers)
-        })
-        return resolve(results)
+          console.log(options.offset, book.id, book.title, book.authors, book.industryIdentifiers);
+        });
+        googleBooks = removeDuplicates(results, results.id);
+        return resolve(googleBooks.slice(0, 9));
       }
     })
   })
